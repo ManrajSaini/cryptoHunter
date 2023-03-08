b5581e7b09e9b7dc3210d43e019f800961b88849
ManrajSaini <manraj.saini123@gmail.com>
2023-03-08 18:35:02 +0530

    First Commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyChYYRfDtes066Ct2MZG4ILIBsFCnwWw6w",
+  authDomain: "crypto-hunter-94316.firebaseapp.com",
+  projectId: "crypto-hunter-94316",
+  storageBucket: "crypto-hunter-94316.appspot.com",
+  messagingSenderId: "877291298274",
+  appId: "1:877291298274:web:79b54873b046d5f9472dbb"
+};
+
+export default firebaseConfig;
